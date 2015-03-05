f848f1e75ba06b95eef562830f96f0e7fcfdc065
Vanessa Perciballi <v.perciballi@gmail.com>
2015-03-05 16:48:53 +0100

    Inserite classi e file mancanti dopo la creazione del nuovo progetto

--- src/com/example/webscrapingclientandroid/MainActivity.java
+++ src/com/example/webscrapingclientandroid/MainActivity.java
@@ -1,17 +1,95 @@
 package com.example.webscrapingclientandroid;
 
-import android.support.v7.app.ActionBarActivity;
+import java.text.ChoiceFormat;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+	
+	private RadioButton rbProfile1, rbProfile2, rbProfile3, rbHotels, rbRestaurants;
+	private Button startButton;
+	public boolean hotel_chosen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //radiobuttons profili
+        rbProfile1 = (RadioButton)findViewById(R.id.radioButton1);
+        rbProfile2 = (RadioButton)findViewById(R.id.radioButton2);
+        rbProfile3 = (RadioButton)findViewById(R.id.radioButton3);
+        
+        //radiobuttons tipologia poi
+        rbHotels = (RadioButton)findViewById(R.id.radioButton4);
+        rbRestaurants = (RadioButton)findViewById(R.id.radioButton4);
+        
+        
+        //bottone submit
+        startButton = (Button)findViewById(R.id.button1);
+        
+        
+        startButton.setOnClickListener(new OnClickListener()
+		{
+			
+			@Override
+			public void onClick(View v)
+			{
+				
+				// controlla il profilo scelto
+				if (rbProfile1.isSelected())
+				{
+					//TODO
+				}
+				else if (rbProfile2.isSelected()) 
+				{
+					
+					//TODO
+				}
+				else if (rbProfile3.isSelected())
+				{
+					//TODO
+				}
+				
+				//TODO metodo per il parsing del profilo
+				
+				
+				
+				//TODO query al db
+				
+				
+				
+				// controllo del tipo di Poi scelto
+				if(rbHotels.isSelected())
+				{
+					hotel_chosen = true;
+				}
+				else if(rbRestaurants.isSelected())
+				{
+					
+				}
+				
+				
+				//passa all'activity contenente la lista dei poi filtrati
+				Intent intent = new Intent(MainActivity.this, PoiList.class);
+				startActivity(intent);
+				finish();
+				
+			}
+		});
+        
+        
+        
+        
     }
 
 
