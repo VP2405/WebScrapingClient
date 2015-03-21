75eab5427e5efc86cb5ca29201d8e4a772d5d31f
Andrea <andrea.alessandro90@gmail.com>
2015-03-21 16:19:01 +0100

    Commentato package com.webscrapingclient.json.map.orderedlist

--- src/com/webscrapingclient/json/map/orderedlist/OrderedListMap.java
+++ src/com/webscrapingclient/json/map/orderedlist/OrderedListMap.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.webscrapingclient.json.map.orderedlist;
 
 import java.util.ArrayList;
@@ -11,77 +8,82 @@ import android.os.Parcelable;
 import com.webscrapingclient.json.map.poi.Restaurant;
 
 /**
+ * Classe relativa al json che contiene la lista degli id dei poi relativi ad un
+ * profilo utente
+ * 
  * @author Vanessa
- *
+ * 
  */
 public class OrderedListMap implements Parcelable {
 	private ArrayList<Integer> ordered_hotels_ids_list;
 	private ArrayList<Integer> ordered_restaurants_ids_list;
-	public ArrayList<Integer> getOrdered_hotels_ids_list()
-	{
+
+	// metodi getter e setter
+	public ArrayList<Integer> getOrdered_hotels_ids_list() {
 		return ordered_hotels_ids_list;
 	}
-	public void setOrdered_hotels_ids_list(ArrayList<Integer> ordered_hotels_ids_list)
-	{
+
+	public void setOrdered_hotels_ids_list(
+			ArrayList<Integer> ordered_hotels_ids_list) {
 		this.ordered_hotels_ids_list = ordered_hotels_ids_list;
 	}
-	public ArrayList<Integer> getOrdered_restaurants_ids_list()
-	{
+
+	public ArrayList<Integer> getOrdered_restaurants_ids_list() {
 		return ordered_restaurants_ids_list;
 	}
-	public void setOrdered_restaurants_ids_list(ArrayList<Integer> ordered_restaurants_ids_list)
-	{
+
+	public void setOrdered_restaurants_ids_list(
+			ArrayList<Integer> ordered_restaurants_ids_list) {
 		this.ordered_restaurants_ids_list = ordered_restaurants_ids_list;
 	}
 
-	
+	protected OrderedListMap(Parcel in) {
+		if (in.readByte() == 0x01) {
+			ordered_hotels_ids_list = new ArrayList<Integer>();
+			in.readList(ordered_hotels_ids_list, Integer.class.getClassLoader());
+		} else {
+			ordered_hotels_ids_list = null;
+		}
+		if (in.readByte() == 0x01) {
+			ordered_restaurants_ids_list = new ArrayList<Integer>();
+			in.readList(ordered_restaurants_ids_list,
+					Integer.class.getClassLoader());
+		} else {
+			ordered_restaurants_ids_list = null;
+		}
+	}
 
-    protected OrderedListMap(Parcel in) {
-        if (in.readByte() == 0x01) {
-            ordered_hotels_ids_list = new ArrayList<Integer>();
-            in.readList(ordered_hotels_ids_list, Integer.class.getClassLoader());
-        } else {
-            ordered_hotels_ids_list = null;
-        }
-        if (in.readByte() == 0x01) {
-            ordered_restaurants_ids_list = new ArrayList<Integer>();
-            in.readList(ordered_restaurants_ids_list, Integer.class.getClassLoader());
-        } else {
-            ordered_restaurants_ids_list = null;
-        }
-    }
+	@Override
+	public int describeContents() {
+		return 0;
+	}
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		if (ordered_hotels_ids_list == null) {
+			dest.writeByte((byte) (0x00));
+		} else {
+			dest.writeByte((byte) (0x01));
+			dest.writeList(ordered_hotels_ids_list);
+		}
+		if (ordered_restaurants_ids_list == null) {
+			dest.writeByte((byte) (0x00));
+		} else {
+			dest.writeByte((byte) (0x01));
+			dest.writeList(ordered_restaurants_ids_list);
+		}
+	}
 
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        if (ordered_hotels_ids_list == null) {
-            dest.writeByte((byte) (0x00));
-        } else {
-            dest.writeByte((byte) (0x01));
-            dest.writeList(ordered_hotels_ids_list);
-        }
-        if (ordered_restaurants_ids_list == null) {
-            dest.writeByte((byte) (0x00));
-        } else {
-            dest.writeByte((byte) (0x01));
-            dest.writeList(ordered_restaurants_ids_list);
-        }
-    }
+	@SuppressWarnings("unused")
+	public static final Parcelable.Creator<OrderedListMap> CREATOR = new Parcelable.Creator<OrderedListMap>() {
+		@Override
+		public OrderedListMap createFromParcel(Parcel in) {
+			return new OrderedListMap(in);
+		}
 
-    @SuppressWarnings("unused")
-    public static final Parcelable.Creator<OrderedListMap> CREATOR = new Parcelable.Creator<OrderedListMap>() {
-        @Override
-        public OrderedListMap createFromParcel(Parcel in) {
-            return new OrderedListMap(in);
-        }
-
-        @Override
-        public OrderedListMap[] newArray(int size) {
-            return new OrderedListMap[size];
-        }
-    };
+		@Override
+		public OrderedListMap[] newArray(int size) {
+			return new OrderedListMap[size];
+		}
+	};
 }
