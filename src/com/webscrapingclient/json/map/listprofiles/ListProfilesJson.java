c0623133aebe8d0bbef5942303bad7296c8203a5
Andrea <andrea.alessandro90@gmail.com>
2015-03-21 16:23:57 +0100

    Commentato il package com.webscrapingclient.json.map.listprofiles

--- src/com/webscrapingclient/json/map/listprofiles/ListProfilesJson.java
+++ src/com/webscrapingclient/json/map/listprofiles/ListProfilesJson.java
@@ -1,43 +1,39 @@
 package com.webscrapingclient.json.map.listprofiles;
 
 /**
- * @author Vanessa 
  * Classe esterna relativa al Json per il profilo utente
+ * 
+ * @author Vanessa
+ * 
  */
 
-public class ListProfilesJson
-{
+public class ListProfilesJson {
 	private int status;
 	private String description;
 	private MapListProfiles map;
 
-	public int getStatus()
-	{
+	// metodi getter e setter
+	public int getStatus() {
 		return status;
 	}
 
-	public void setStatus(int status)
-	{
+	public void setStatus(int status) {
 		this.status = status;
 	}
 
-	public String getDescription()
-	{
+	public String getDescription() {
 		return description;
 	}
 
-	public void setDescription(String description)
-	{
+	public void setDescription(String description) {
 		this.description = description;
 	}
 
-	public MapListProfiles getMap()
-	{
+	public MapListProfiles getMap() {
 		return map;
 	}
 
-	public void setMap(MapListProfiles map)
-	{
+	public void setMap(MapListProfiles map) {
 		this.map = map;
 	}
 
--- src/com/webscrapingclient/json/map/listprofiles/MapListProfiles.java
+++ src/com/webscrapingclient/json/map/listprofiles/MapListProfiles.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.webscrapingclient.json.map.listprofiles;
 
 import java.util.ArrayList;
@@ -12,83 +9,94 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 /**
- * @author Vanessa Classe per il recupero della lista dei profili dal parsing
- *         del Json relativo Implementa Parcelable in modo da poter passare una
- *         sua istanza dalla classe SplashScreenActivity, dove viene effettuato
- *         il parsing e creata la lista, alla classe MainActivity in cui la
- *         lista viene utilizzata per popolare lo Spinner per la scelta del
- *         profilo per il recupero della lista dei poi ordinati.
- *
+ * Classe per il recupero della lista dei profili dal parsing del Json relativo
+ * Implementa Parcelable in modo da poter passare una sua istanza dalla classe
+ * SplashScreenActivity, dove viene effettuato il parsing e creata la lista,
+ * alla classe MainActivity in cui la lista viene utilizzata per popolare lo
+ * Spinner per la scelta del profilo per il recupero della lista dei poi
+ * ordinati.
+ * 
+ * @author Vanessa
+ * 
  */
-public class MapListProfiles implements Parcelable
-{
+public class MapListProfiles implements Parcelable {
 
 	private ArrayList<Integer> all_profiles_ids;
 
-	
-	public MapListProfiles(ArrayList<Integer> all_profiles_ids)
-	{
+	/**
+	 * Costruttore per la classe MapListProfiles
+	 * 
+	 * @param all_profiles_ids
+	 */
+	public MapListProfiles(ArrayList<Integer> all_profiles_ids) {
 		super();
 		this.all_profiles_ids = all_profiles_ids;
 	}
 
-	public void addProfileId(CommercialProfile profile){
+	/**
+	 * Aggiunge l'id del profilo passato alla lista degli id
+	 * 
+	 * @param profile
+	 */
+	public void addProfileId(CommercialProfile profile) {
 		all_profiles_ids.add(profile.getId());
 	}
-	
-	public ArrayList<Integer> getAllProfilesIds()
-	{
+
+	/**
+	 * Restituisce la lista degli id di tutti i profili presenti
+	 * 
+	 * @return
+	 */
+	public ArrayList<Integer> getAllProfilesIds() {
 		return all_profiles_ids;
 	}
 
-	public void setAllProfilesIds(ArrayList<Integer> allProfilesIds)
-	{
+	/**
+	 * Imposta come lista dei profili quella passata al metodo
+	 * 
+	 * @param allProfilesIds
+	 */
+	public void setAllProfilesIds(ArrayList<Integer> allProfilesIds) {
 		this.all_profiles_ids = allProfilesIds;
 	}
 
-	protected MapListProfiles(Parcel in)
-	{
-		if (in.readByte() == 0x01)
-		{
+	/**
+	 * Costruttore di MapListProfiles per l'interfaccia Parcelable
+	 * 
+	 * @param in
+	 */
+	protected MapListProfiles(Parcel in) {
+		if (in.readByte() == 0x01) {
 			all_profiles_ids = new ArrayList<Integer>();
 			in.readList(all_profiles_ids, Integer.class.getClassLoader());
-		} else
-		{
+		} else {
 			all_profiles_ids = null;
 		}
 	}
 
 	@Override
-	public int describeContents()
-	{
+	public int describeContents() {
 		return 0;
 	}
 
 	@Override
-	public void writeToParcel(Parcel dest, int flags)
-	{
-		if (all_profiles_ids == null)
-		{
+	public void writeToParcel(Parcel dest, int flags) {
+		if (all_profiles_ids == null) {
 			dest.writeByte((byte) (0x00));
-		} else
-		{
+		} else {
 			dest.writeByte((byte) (0x01));
 			dest.writeList(all_profiles_ids);
 		}
 	}
 
-
-	public static final Parcelable.Creator<MapListProfiles> CREATOR = new Parcelable.Creator<MapListProfiles>()
-	{
+	public static final Parcelable.Creator<MapListProfiles> CREATOR = new Parcelable.Creator<MapListProfiles>() {
 		@Override
-		public MapListProfiles createFromParcel(Parcel in)
-		{
+		public MapListProfiles createFromParcel(Parcel in) {
 			return new MapListProfiles(in);
 		}
 
 		@Override
-		public MapListProfiles[] newArray(int size)
-		{
+		public MapListProfiles[] newArray(int size) {
 			return new MapListProfiles[size];
 		}
 	};
